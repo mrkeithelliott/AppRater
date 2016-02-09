a0836ebf208e2d4ccc088871a69f0101f227d2f3
Keith Elliott <keith@gittie.com>
2016-02-09 07:23:47 -0500

    initial project commit

---
+++ Build/Intermediates/AppRaterSample.build/Debug-iphonesimulator/apprater.build/DerivedSources/apprater_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char appraterVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:apprater  PROJECT:AppRaterSample-1" "\n"; const double appraterVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ apprater/apprater.h
@@ -0,0 +1,19 @@
+//
+//  apprater.h
+//  apprater
+//
+//  Created by Keith Elliott on 2/9/16.
+//  Copyright © 2016 GittieLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for apprater.
+FOUNDATION_EXPORT double appraterVersionNumber;
+
+//! Project version string for apprater.
+FOUNDATION_EXPORT const unsigned char appraterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <apprater/PublicHeader.h>
+
+
